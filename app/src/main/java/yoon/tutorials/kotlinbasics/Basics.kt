fc7a0a6c0254b6fa98e7526d6ee0e75a562928e0
SuHwanYoon <ehtldy@naver.com>
2024-08-05 21:02:06 +0900

    add kotlin number syntax

--- app/src/main/java/yoon/tutorials/kotlinbasics/Basics.kt
+++ app/src/main/java/yoon/tutorials/kotlinbasics/Basics.kt
@@ -4,9 +4,12 @@ fun main(){
     val number1 = 1
     //number1 = 2
 
-    var myage = 33
+    var myage : Byte= 33
     println(myage)
     myage = 34
     println(myage)
-
+    var bNumber : Long = 2134234324;
+    println(bNumber)
+    var exage : UShort = 50u;
+    println(exage)
 }

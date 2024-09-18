607380be62aac691d66d56bb7b4b9a46281e3280
SuHwanYoon <ehtldy@naver.com>
2024-09-18 18:54:50 +0900

    add by keyword concepts

--- app/src/main/java/yoon/tutorials/kotlinbasics/rememberstate/RememberActivity.kt
+++ app/src/main/java/yoon/tutorials/kotlinbasics/rememberstate/RememberActivity.kt
@@ -10,8 +10,11 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import yoon.tutorials.kotlinbasics.rememberstate.ui.theme.KotlinBasicsTheme
@@ -37,24 +40,34 @@ class RememberActivity : ComponentActivity() {
     @Composable
     fun CaptainGame() {
         //remember로 compose상태를 저장, muatbleStateOf로 상태를 관찰하여 컴포저블 함수 재구성
-        val treasuresFound = remember {
-            mutableStateOf(0)
-        }
+        //val treasuresFound = remember { mutableStateOf(0) }
+        //by keyword를 사용해서 직접 접근가능 변수값 변경을 위해서 var사용
+        var treasuresFound by remember { mutableIntStateOf(0) }
         //초기 direction text 는 North
-        val direction = remember {
-            mutableStateOf("North")
-        }
+        val direction = remember { mutableStateOf("North") }
+        //초기 변수값은 empty String
+        val stormOrTreasure = remember { mutableStateOf("") }
         Column {
             //remember 변수값에 접근하는 텍스트
-            Text(text = "Treasures Found: ${treasuresFound.value}")
+//            Text(text = "Treasures Found: ${treasuresFound.value}")
+            //by keyword가 있기때문에 직접접근가능 {} 삭제가능
+            Text(text = "Treasures Found: ${treasuresFound}")
             Text(text = "Current Direction: ${direction.value}")
+            //stormOrTreasure를 만났을때 나타나는 text
+            Text(text = "${stormOrTreasure.value}")
             //동쪽 버튼
             Button(onClick = {
                 //버튼 클릭시 direction값을 변경
                 direction.value = "East"
-                //버튼 클릭시 50%의 확률로 true가 나오면 treasuresFound값이 증가
+                //버튼 클릭시 50%의 확률로 true가 나오면
                 if (Random.nextBoolean()) {
-                    treasuresFound.value++
+                    //treasuresFound값이 증가 + text표시
+                    treasuresFound++
+                    stormOrTreasure.value = "We found a treasure!"
+                } else {
+                    //kotlin은 Ternary Operation을 사용하지 않음
+                    //treasure를 못찾았을 경우 text
+                    stormOrTreasure.value = "It's a storm! Be careful!"
                 }
             }) {
                 Text(text = "Change to East")
@@ -63,9 +76,15 @@ class RememberActivity : ComponentActivity() {
             Button(onClick = {
                 //버튼 클릭시 direction값을 변경
                 direction.value = "West"
-                //버튼 클릭시 50%의 확률로 true가 나오면 treasuresFound값이 증가
+                //버튼 클릭시 50%의 확률로 true가 나오면
                 if (Random.nextBoolean()) {
-                    treasuresFound.value++
+                    //treasuresFound값이 증가 + text표시
+                    treasuresFound++
+                    stormOrTreasure.value = "We found a treasure!"
+                } else {
+                    //kotlin은 Ternary Operation을 사용하지 않음
+                    //treasure를 못찾았을 경우 text
+                    stormOrTreasure.value = "It's a storm! Be careful!"
                 }
             }) {
                 Text(text = "Change to West")
@@ -74,9 +93,15 @@ class RememberActivity : ComponentActivity() {
             Button(onClick = {
                 //버튼 클릭시 direction값을 변경
                 direction.value = "North"
-                //버튼 클릭시 50%의 확률로 true가 나오면 treasuresFound값이 증가
+                //버튼 클릭시 50%의 확률로 true가 나오면
                 if (Random.nextBoolean()) {
-                    treasuresFound.value++
+                    //treasuresFound값이 증가 + text표시
+                    treasuresFound++
+                    stormOrTreasure.value = "We found a treasure!"
+                } else {
+                    //kotlin은 Ternary Operation을 사용하지 않음
+                    //treasure를 못찾았을 경우 text
+                    stormOrTreasure.value = "It's a storm! Be careful!"
                 }
             }) {
                 Text(text = "Change to North")
@@ -85,9 +110,15 @@ class RememberActivity : ComponentActivity() {
             Button(onClick = {
                 //버튼 클릭시 direction값을 변경
                 direction.value = "South"
-                //버튼 클릭시 50%의 확률로 true가 나오면 treasuresFound값이 증가
+                //버튼 클릭시 50%의 확률로 true가 나오면
                 if (Random.nextBoolean()) {
-                    treasuresFound.value++
+                    //treasuresFound값이 증가 + text표시
+                    treasuresFound++
+                    stormOrTreasure.value = "We found a treasure!"
+                } else {
+                    //kotlin은 Ternary Operation을 사용하지 않음
+                    //treasure를 못찾았을 경우 text
+                    stormOrTreasure.value = "It's a storm! Be careful!"
                 }
             }) {
                 Text(text = "Change to South")
